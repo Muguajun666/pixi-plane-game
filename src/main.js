6e32d5c90438f358ed4d5d0eaf189548819a1979
Muguajun666 <liushulialen@qq.com>
2024-05-09 11:41:25 +0800

    feat: init game renderer

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createRenderer } from 'vue';
+import App from './App.vue';
+import { Application, Graphics } from 'pixi.js';
 
-createApp(App).mount('#app')
+// init game
+const game = new Application({
+  width: 500,
+  height: 500,
+});
+
+document.body.append(game.view);
+
+const renderer = createRenderer({
+  createElement(type) {
+    let element;
+    if (type === 'rect') {
+      element = new Graphics();
+      element.beginFill(0xff0000);
+      element.drawRect(0, 0, 50, 50);
+      element.endFill();
+    }
+    return element;
+  },
+  insert(el, parent) {
+    parent.addChild(el)
+  },
+  // setElementText(node, text) {
+  // }
+});
+
+renderer.createApp(App).mount(game.stage)
